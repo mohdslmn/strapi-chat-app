0a687f2ea77eaed6dc2c333605a60c5b9351b540
Md salman <99859514+mohdslmn@users.noreply.github.com>
2024-05-12 01:00:26 +0530

    Updated

--- config/database.js
+++ config/database.js
@@ -1,9 +1,53 @@
+
 const path = require('path');
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
+    // mysql: {
+    //   connection: {
+    //     connectionString: env('DATABASE_URL'),
+    //     host: env('DATABASE_HOST', 'localhost'),
+    //     port: env.int('DATABASE_PORT', 3306),
+    //     database: env('DATABASE_NAME', 'strapi'),
+    //     user: env('DATABASE_USERNAME', 'strapi'),
+    //     password: env('DATABASE_PASSWORD', 'strapi'),
+    //     ssl: env.bool('DATABASE_SSL', false) && {
+    //       key: env('DATABASE_SSL_KEY', undefined),
+    //       cert: env('DATABASE_SSL_CERT', undefined),
+    //       ca: env('DATABASE_SSL_CA', undefined),
+    //       capath: env('DATABASE_SSL_CAPATH', undefined),
+    //       cipher: env('DATABASE_SSL_CIPHER', undefined),
+    //       rejectUnauthorized: env.bool(
+    //         'DATABASE_SSL_REJECT_UNAUTHORIZED',
+    //         true
+    //       ),
+    //     },
+    //   },
+    //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    // },
+    // mysql2: {
+    //   connection: {
+    //     host: env('DATABASE_HOST', 'localhost'),
+    //     port: env.int('DATABASE_PORT', 3306),
+    //     database: env('DATABASE_NAME', 'strapi'),
+    //     user: env('DATABASE_USERNAME', 'strapi'),
+    //     password: env('DATABASE_PASSWORD', 'strapi'),
+    //     ssl: env.bool('DATABASE_SSL', false) && {
+    //       key: env('DATABASE_SSL_KEY', undefined),
+    //       cert: env('DATABASE_SSL_CERT', undefined),
+    //       ca: env('DATABASE_SSL_CA', undefined),
+    //       capath: env('DATABASE_SSL_CAPATH', undefined),
+    //       cipher: env('DATABASE_SSL_CIPHER', undefined),
+    //       rejectUnauthorized: env.bool(
+    //         'DATABASE_SSL_REJECT_UNAUTHORIZED',
+    //         true
+    //       ),
+    //     },
+    //   },
+    //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    // },
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
@@ -13,114 +57,37 @@ module.exports = ({ env }) => {
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', true) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-          ca: env('DATABASE_SSL_CA', ''),
-          key: env('DATABASE_SSL_KEY', ''),
-          cert: env('DATABASE_SSL_CERT', ''),
-          passphrase: env('DATABASE_SSL_PASSPHRASE', ''),
+          key: env('DATABASE_SSL_KEY', ""),
+          cert: env('DATABASE_SSL_CERT', ""),
+          ca: env('DATABASE_SSL_CA', ""),
+          capath: env('DATABASE_SSL_CAPATH', ""),
+          cipher: env('DATABASE_SSL_CIPHER',""),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
+    // sqlite: {
+    //   connection: {
+    //     filename: path.join(
+    //       __dirname,
+    //       '..',
+    //       env('DATABASE_FILENAME', '.tmp/data.db')
+    //     ),
+    //   },
+    //   useNullAsDefault: true,
+    // },
   };
 
   return {
-    defaultConnection: 'default',
-    connections,
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
   };
 };
-
-
-// const path = require('path');
-
-// module.exports = ({ env }) => {
-//   const client = env('DATABASE_CLIENT', 'sqlite');
-
-//   const connections = {
-//     // mysql: {
-//     //   connection: {
-//     //     connectionString: env('DATABASE_URL'),
-//     //     host: env('DATABASE_HOST', 'localhost'),
-//     //     port: env.int('DATABASE_PORT', 3306),
-//     //     database: env('DATABASE_NAME', 'strapi'),
-//     //     user: env('DATABASE_USERNAME', 'strapi'),
-//     //     password: env('DATABASE_PASSWORD', 'strapi'),
-//     //     ssl: env.bool('DATABASE_SSL', false) && {
-//     //       key: env('DATABASE_SSL_KEY', undefined),
-//     //       cert: env('DATABASE_SSL_CERT', undefined),
-//     //       ca: env('DATABASE_SSL_CA', undefined),
-//     //       capath: env('DATABASE_SSL_CAPATH', undefined),
-//     //       cipher: env('DATABASE_SSL_CIPHER', undefined),
-//     //       rejectUnauthorized: env.bool(
-//     //         'DATABASE_SSL_REJECT_UNAUTHORIZED',
-//     //         true
-//     //       ),
-//     //     },
-//     //   },
-//     //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-//     // },
-//     // mysql2: {
-//     //   connection: {
-//     //     host: env('DATABASE_HOST', 'localhost'),
-//     //     port: env.int('DATABASE_PORT', 3306),
-//     //     database: env('DATABASE_NAME', 'strapi'),
-//     //     user: env('DATABASE_USERNAME', 'strapi'),
-//     //     password: env('DATABASE_PASSWORD', 'strapi'),
-//     //     ssl: env.bool('DATABASE_SSL', false) && {
-//     //       key: env('DATABASE_SSL_KEY', undefined),
-//     //       cert: env('DATABASE_SSL_CERT', undefined),
-//     //       ca: env('DATABASE_SSL_CA', undefined),
-//     //       capath: env('DATABASE_SSL_CAPATH', undefined),
-//     //       cipher: env('DATABASE_SSL_CIPHER', undefined),
-//     //       rejectUnauthorized: env.bool(
-//     //         'DATABASE_SSL_REJECT_UNAUTHORIZED',
-//     //         true
-//     //       ),
-//     //     },
-//     //   },
-//     //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-//     // },
-//     postgres: {
-//       connection: {
-//         connectionString: env('DATABASE_URL'),
-//         host: env('DATABASE_HOST'),
-//         port: env.int('DATABASE_PORT'),
-//         database: env('DATABASE_NAME'),
-//         user: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-//         ssl: env.bool('DATABASE_SSL', true) && {
-//           key: env('DATABASE_SSL_KEY', ""),
-//           cert: env('DATABASE_SSL_CERT', ""),
-//           ca: env('DATABASE_SSL_CA', ""),
-//           capath: env('DATABASE_SSL_CAPATH', ""),
-//           cipher: env('DATABASE_SSL_CIPHER',""),
-//           rejectUnauthorized: env.bool(
-//             'DATABASE_SSL_REJECT_UNAUTHORIZED',
-//             true
-//           ),
-//         },
-//         schema: env('DATABASE_SCHEMA', 'public'),
-//       },
-//       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-//     },
-//     // sqlite: {
-//     //   connection: {
-//     //     filename: path.join(
-//     //       __dirname,
-//     //       '..',
-//     //       env('DATABASE_FILENAME', '.tmp/data.db')
-//     //     ),
-//     //   },
-//     //   useNullAsDefault: true,
-//     // },
-//   };
-
-//   return {
-//     connection: {
-//       client,
-//       ...connections[client],
-//       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-//     },
-//   };
-// };
